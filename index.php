4a02efb97ff24db5d16217a32ed68beacc316255
OtaSteven <sho8517@gmail.com>
2022-11-21 15:35:13 -0800

    Remove home.php as that should be index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+require "db_conn.php"; // Making sure this file has the connection to our database
+?>
 <!DOCTYPE html>
 <html>
 
@@ -12,6 +16,36 @@
             <button id="submit" onclick="printUserText();">Submit</button>
             <p id="userShowName"></p>
         </form>
+        <table>
+            <tr>
+                <th>Show_Table</th>
+            </tr>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Anime Season</th>
+                <th>Anime Year</th>
+                <th>Cartoon Network</th>
+                <th>Cartoon Release</th>
+            </tr>
+            <tr id="demo">
+            <?php
+                $search_result = mysqli_query($conn,"SELECT * FROM show_table");
+                while($data = mysqli_fetch_array($search_result)) 
+                { ?>
+                    <tr>
+                        <td><?php echo $data['show_id']; ?></td>
+                        <td><?php echo $data['name']; ?></td>
+                        <td><?php echo $data['show_Type']; ?></td>
+                        <td><?php echo $data['anime_Season']; ?></td>
+                        <td><?php echo $data['anime_Year']; ?></td>
+                        <td><?php echo $data['cartoon_Network']; ?></td>
+                        <td><?php echo $data['cartoon_Release']; ?></td>
+                    </tr>
+                <?php } ?>
+            </tr>
+        </table>
     </body>
 </html>
 
