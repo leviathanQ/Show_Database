3098f4f4c7af6cd2046f6f43255d4913ecb611f3
OtaSteven <91758451+OtaSteven@users.noreply.github.com>
2022-11-30 16:46:12 -0800

    Added in comments

--- insertData.php
+++ insertData.php
@@ -3,21 +3,26 @@
 	require 'db_conn.php';
 		
 	$stmt = mysqli_stmt_init($conn);
+
+	//Creating a prepared statement for when user want to insert into user's table.
 	mysqli_stmt_prepare($stmt,  "INSERT INTO user_input SELECT show_id FROM show_table WHERE name = (?)");
 
+	//Making sure that $show_name is binded or categories as a string variable for the prepared statement
 	mysqli_stmt_bind_param($stmt, "s", $show_Name);
 
+	//Getting user's input from an input named 'userInput'
 	$show_Name = $_REQUEST['userInput'];
 
-	echo gettype($show_Name);
+	//echo gettype($show_Name);
 	
+	//Executing the prepared statement with user's input
 	mysqli_stmt_execute($stmt);
 
 	//echo "<p>RECORD INSERTED SUCCESSFULLY</p>";
 	
-	header("Location: home.php");	
+	header("Location: home.php");	//Returning back to home screen
 
-	mysqli_stmt_close($stmt);
+	mysqli_stmt_close($stmt);	//closing off the prepared statement
 
 	mysqli_close($conn);
 ?>
