292e0c79a8d710007108eb90f2f20014c0483f94
OtaSteven <91758451+OtaSteven@users.noreply.github.com>
2022-11-30 16:38:17 -0800

    Added in comments that explains more about what certain line of code(s) does

--- database/show.sql
+++ database/show.sql
@@ -1,5 +1,5 @@
 /*
- * Connecting to DB:
+ * Connecting/setting DB to ecs server in DataGrip:
  * 1) Create a new Data Source (Mysql)
  * 2) Go to the ssh tab and check the box and then click on the three dots to edit properties
  * 3) For ssh host put in: ecs-pd-proj-db.ecs.csus.edu and for user and password, it will be your school user and pass
@@ -13,6 +13,7 @@ SHOW TABLES;
 SHOW TRIGGERS;
 
 DROP TABLE show_table;
+
 CREATE TABLE show_table(
     show_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -30,40 +31,42 @@ DROP TRIGGER on_Update;
 DROP TRIGGER on_Insert;
 
 DELIMITER $$
-    CREATE PROCEDURE anime_Insert(show_Name VARCHAR(255), ani_Season ENUM
-        ('Spring', 'Summer', 'Fall', 'Winter'), ani_Year INT)
-    BEGIN
-        INSERT INTO show_table (name, show_Type, anime_Season, anime_Year) VALUES
-        (show_Name, 'Anime', ani_Season, ani_Year);
-    end $$
 
-    CREATE PROCEDURE cartoon_Insert(show_Name VARCHAR(255), cart_Network
-        VARCHAR(255), cart_Release INT)
-    BEGIN
-        INSERT INTO show_table (name, show_Type, carton_Network,
-        cartoon_Release) VALUES (show_Name, 'Cartoon', cart_Network,
-                                 cart_Release);
-    end $$
-
-CREATE TRIGGER on_Update
-    BEFORE UPDATE ON show_table
-    FOR EACH ROW
-    BEGIN
-        IF new.name IN (SELECT name FROM show_table WHERE name = new.name
-                                                    AND show_Type = 'Anime')
+    /*
+     * Creating anime_Insert and cartoon_Insert PROCEDURE to easily insert data into the table.
+     */
+     CREATE PROCEDURE anime_Insert(show_Name VARCHAR(255), ani_Season ENUM('Spring', 'Summer', 'Fall', 'Winter'), ani_Year INT)
+     BEGIN
+        INSERT INTO show_table (name, show_Type, anime_Season, anime_Year) 
+        VALUES (show_Name, 'Anime', ani_Season, ani_Year);
+     END $$
+     
+     CREATE PROCEDURE cartoon_Insert(show_Name VARCHAR(255), cart_Network VARCHAR(255), cart_Release INT)
+     BEGIN
+        INSERT INTO show_table (name, show_Type, carton_Network, cartoon_Release) 
+        VALUES (show_Name, 'Cartoon', cart_Network, cart_Release);
+     END $$
+     /*
+     * Trigger that make sure that no shows have the same name
+     */
+     CREATE TRIGGER on_Update
+     BEFORE UPDATE ON show_table
+     FOR EACH ROW
+     BEGIN
+        IF new.name IN (SELECT name FROM show_table WHERE name = new.name AND show_Type = 'Anime')
             THEN SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'That name is already in the table and it\'s
-classified as an anime type';
+            SET MESSAGE_TEXT = 'That name is already in the table and it\'s classified as an anime type';
         END IF;
-        IF new.name IN (SELECT name FROM show_table WHERE name = new.name
-                                                    AND show_Type = 'Cartoon')
+        IF new.name IN (SELECT name FROM show_table WHERE name = new.name AND show_Type = 'Cartoon')
             THEN SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'That name is already in the table and it\'s
-classified as an cartoon type';
+            SET MESSAGE_TEXT = 'That name is already in the table and it\'s classified as an cartoon type';
         END IF;
     END $$
-
-CREATE TRIGGER on_Insert
+    
+    /*
+    * A trigger that make sure that before inserting into the table that the name is not already taken
+    */
+    CREATE TRIGGER on_Insert
     BEFORE INSERT ON show_table
     FOR EACH ROW
     BEGIN
@@ -72,8 +75,10 @@ CREATE TRIGGER on_Insert
             SET MESSAGE_TEXT = 'That name is already taken';
         END IF;
     END $$
+
 DELIMITER ;
 
+/* Inserting data into our show table */
 CALL anime_Insert('Bleach', 'Fall', 2004);
 CALL anime_Insert('Demon Slayer', 'Spring', 2019);
 CALL anime_Insert('Detective Conan', 'Winter', 1994);
@@ -106,8 +111,5 @@ CALL cartoon_Insert('The Simpsons','Fox',1989);
 CALL cartoon_Insert('Tom and Jerry','CBS',1994);
 CALL cartoon_Insert('Ultimate Spider-Man','Disney XD',2012);
 
-
-TRUNCATE show_table;
-SELECT * FROM show_table;
-
-UPDATE show_table SET name = 'test2' WHERE show_id = 1;
+SELECT * 
+FROM show_table;
