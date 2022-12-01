6282f23e61a435ec8c614e22b47579195eaa8cbf
OtaSteven <91758451+OtaSteven@users.noreply.github.com>
2022-11-30 16:41:55 -0800

    Added in comments to explain some lines of codes

--- clearUserTable.php
+++ clearUserTable.php
@@ -2,10 +2,11 @@
 
 	require 'db_conn.php';
 
-	$sql = "DELETE FROM user_table WHERE show_id > 0";
-	if (isset($_POST['clearTable']))
+	$sql = "DELETE FROM user_table WHERE show_id > 0";	//Initializing sql code to run
+	
+	if (isset($_POST['clearTable']))			//checkign to see if the clear button is ever press
 	{
-		if (mysqli_query($conn, $sql))
+		if (mysqli_query($conn, $sql))			//running the sql code and will output if it succeeds or not
 		{
 			echo "Records were deleted successfully";
 		}
@@ -14,6 +15,6 @@
 			echo "ERROR";
 		}
 	}
-	header("Location: home.php");
+	header("Location: home.php");				//returns back to home screen
 	mysqli_close($conn);
 ?>
